d974be7643b07617c183391f4024c92b6b42c0ec
enjoroge <njrogeelizabeth53@gmail.com>
2024-03-26 22:57:36 +0300

    control flow

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
-
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(FEET){
+    //write your code here!
+    if (FEET<=400) {
+      return "This one is on me!";
+    } else if (FEET<=2000) {
+      return "That will be twenty bucks.";
+    } else if (FEET<=2500) {
+      return "I will gladly take your thirty bucks.";
+    } else {
+      return "No can do.";
+    }
+  }
+  
+  function ternaryCheckCity(city){
+    return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+  }
+  
+  function switchOnCharmFromTip(tip){
+    switch (tip) {
+      case 'generous':
+        return 'Thank you so much.';
+      case 'not as generous':
+        return "Thank you.";
+      default:
+        return 'Bye.';
+    }
+  }
+  
